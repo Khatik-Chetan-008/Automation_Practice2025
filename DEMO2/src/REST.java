789937fce733f32b89e7336e0afb284664427381
cvkhatik <cvkhatik@HW1000000689.NCODECORP.IN>
2025-07-21 16:09:49 +0530

    first commit

---
+++ DEMO2/src/REST.java
@@ -0,0 +1,14 @@
+
+
+public class REST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=3;
+ System.out.println(++i);
+ System.out.println(++i);
+ System.out.println(++i);
+ System.out.println(i++);
+	}
+
+}
