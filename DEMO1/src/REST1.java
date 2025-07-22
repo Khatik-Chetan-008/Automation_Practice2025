8a5f678517cdc97ff1ab524166008b16a51965ee
cvkhatik <cvkhatik@HW1000000689.NCODECORP.IN>
2025-07-22 12:13:28 +0530

    seconmd commit

--- DEMO1/src/REST1.java
+++ DEMO1/src/REST1.java
@@ -4,7 +4,7 @@ public class REST1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int i=3;
+int i=5;
  System.out.println(++i);
  System.out.println(++i);
  System.out.println(++i);
